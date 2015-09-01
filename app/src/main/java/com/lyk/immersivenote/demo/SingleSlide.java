554ad34208580c0fb03ee79b580effba1c0dc404
moniren <390769017qq@gmail.com>
2015-09-01 22:56:41 +0800

    added the missing class

---
+++ app/src/main/java/com/lyk/immersivenote/demo/SingleSlide.java
@@ -0,0 +1,46 @@
+package com.lyk.immersivenote.demo;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by John on 2015/9/1.
+ */
+public class SingleSlide extends Fragment {
+
+    private static final String ARG_LAYOUT_RES_ID = "layoutResId";
+
+    public static SingleSlide newInstance(int layoutResId) {
+        SingleSlide sampleSlide = new SingleSlide();
+
+        Bundle args = new Bundle();
+        args.putInt(ARG_LAYOUT_RES_ID, layoutResId);
+        sampleSlide.setArguments(args);
+
+        return sampleSlide;
+    }
+
+    private int layoutResId;
+
+    public SingleSlide() {}
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if(getArguments() != null && getArguments().containsKey(ARG_LAYOUT_RES_ID))
+            layoutResId = getArguments().getInt(ARG_LAYOUT_RES_ID);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(layoutResId, container, false);
+    }
+
+}
